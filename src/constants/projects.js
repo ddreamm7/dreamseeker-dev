d2532a0065a0142257a844bc6b9cbb98494ee4b4
ddreamm7 <aalonsoopg@gmail.com>
2026-03-07 09:20:52 -0500

    Update projects.js

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -8,22 +8,4 @@ export const projects = [
     github: '',
     download: 'https://mega.nz/folder/0v123YLb#nIElGcv92NQ6RBewh3--yQ',
   },
-  {
-    title: 'Dark Forest',
-    alt: 'Dark Forest logo',
-    engine: 'Unity 6',
-    description: 'projects.list.dark_forest.description',
-    logo: new URL('@/assets/images/projects/darkforest-logo.webp', import.meta.url).href,
-    github: '',
-    download: 'https://mega.nz/folder/0v123YLb#nIElGcv92NQ6RBewh3--yQ',
-  },
-  {
-    title: 'Dark Forest',
-    alt: 'Dark Forest logo',
-    engine: 'Unity 6',
-    description: 'projects.list.dark_forest.description',
-    logo: new URL('@/assets/images/projects/darkforest-logo.webp', import.meta.url).href,
-    github: '',
-    download: 'https://mega.nz/folder/0v123YLb#nIElGcv92NQ6RBewh3--yQ',
-  },
 ];
